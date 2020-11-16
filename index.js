959817d92646fee9ee5eb97bddcbeebcf2b104df
Arpit Mohan <arpit@appsmith.com>
2020-11-16 20:59:37 +0530

    Adding context & payload

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const core = require('@actions/core');
 // const wait = require('./wait');
-
+const axios = require("axios");
+const github = require("@actions/github");
 
 // most @actions toolkit packages have async methods
 async function run() {
   try {
     const prId = core.getInput('pr-id');
     core.info(`PR ID: ${prId}`);
-    console.log()
+    
+    const context = github.context
+    const payload = context.payload;
+    core.info(`Payload: ${payload}`);
+
+    // const response = await instance.request(requestData)
     // core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
     // await wait(parseInt(ms));
     // core.info((new Date()).toTimeString());
